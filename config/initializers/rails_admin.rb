3c1ba74590b6826767d10ac0e1d7e76789fad36b
Ali-crewlogix <ali.ahmed@crewlogix.com>
2021-05-06 09:18:05 -0700

    flash hash removed

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -18,7 +18,7 @@ RailsAdmin.config do |config|
     unless current_user && current_user.is_admin?
       redirect_to(
         main_app.root_path,
-        flash[:alert] = "You are not permitted to view this page"
+        alert: "You are not permitted to view this page"
       )
     end
   end
